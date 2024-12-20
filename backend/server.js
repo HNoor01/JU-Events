75a3d1da3bce488879a808aa501d8ba6d1486077
RayahYahya <rayahyahya00@gmail.com>
2024-12-20 19:36:22 +0300

    backend folder added

---
+++ backend/server.js
@@ -0,0 +1,34 @@
+const mysql = require('mysql2');
+const express = require('express'); 
+const app = express();
+const port = 5000; 
+
+
+app.get('/', (req, res) => {
+  res.send('Backend is  working!');
+});
+
+// بدء تشغيل السيرفر
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
+
+
+
+
+const db = mysql.createConnection({
+    host: '127.0.0.1:3306',
+    user: 'root',  
+    password: '', 
+    database: 'juevents',  
+  });
+  
+ 
+  db.connect((err) => {
+    if (err) {
+      console.error('Error connecting to the database:', err.stack);
+      return;
+    }
+    console.log('Connected to the database');
+  });
+  
