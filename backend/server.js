e202a1d6834c79f731470d444e9b2e3e23a3d77c
RayahYahya <rayahyahya00@gmail.com>
2024-12-24 22:28:21 +0300

    Add files via upload

--- backend/server.js
+++ backend/server.js
@@ -2,8 +2,8 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const sequelize = require('./models/database');
 const eventRoutes = require('./routes/eventRoutes');
-const adminRoutes = require('./routes/adminRoutes'); 
-const studentsRoutes = require('./routes/studentsRoutes'); 
+const adminRoutes = require('./routes/adminRoutes');
+const studentsRoutes = require('./routes/studentsRoutes.js');
 
 const app = express();
 app.use(bodyParser.json());
@@ -12,23 +12,29 @@ app.get('/', (req, res) => {
   res.send('Welcome to the Event Management API!');
 });
 
-
 app.use('/api/admin', adminRoutes);
-
 app.use('/api/event-requests', eventRoutes);
-
 app.use('/api/students', studentsRoutes);
 
 const PORT = 3000;
+
 sequelize.authenticate()
   .then(() => {
     console.log('Database connected!');
-    sequelize.sync({ alter: true }).then(() => {
-      app.listen(PORT, () => {
-        console.log(`Server is running on http://localhost:${PORT}`);
+    
+    // إضافة هذه الخطوة لمزامنة الجداول
+    sequelize.sync({ alter: true })  // { alter: true } سيقوم بتحديث الجداول إذا كان هناك تغيير في النموذج
+      .then(() => {
+        console.log('Tables synced with the database!');
+        app.listen(PORT, () => {
+          console.log(`Server is running on http://localhost:${PORT}`);
+        });
+      })
+      .catch(err => {
+        console.error('Error syncing tables:', err);
       });
-    });
   })
   .catch(error => {
     console.error('Failed to connect to the database:', error);
   });
+  
